8b31b7905ec02bc59392821c7c3f450e60c7424a
Greg Kofman <gregkofman@Gregs-MacBook-Pro.local>
2018-06-05 14:43:19 +0530

    first page

---
+++ mHealth/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  mHealth
+//
+//  Created by Greg Kofman on 6/3/18.
+//  Copyright © 2018 Greg Kofman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //MARK: Actions
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
+
